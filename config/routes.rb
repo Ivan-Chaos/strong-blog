ad4dca3191590c9fc07317c989685f9ed63ccc7b
Ivan Chaus <ivan13oct@gmail.com>
2021-05-10 03:13:22 +0300

    Getting started(not generated)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #get '/hello', to: 'home#hello'
+  
+  get '/posts', to: 'posts#index'
+  post '/posts', to: 'posts#create'
+  get '/posts/new', to: 'posts#new', as: :new_post
+  get '/posts/:id', to: 'posts#show', as: :post
+  get '/posts/:id/edit', to: 'posts#edit', as: :edit_post
+  patch '/posts/:id', to: 'posts#update'
+
+
+  root to: redirect('/posts')
+
 end
